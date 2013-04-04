60a4fb10a09fc12226aa05bd050551e65a6826ac
Jon Schneider <jon@jon-Alienware-X51.(none)>
2013-04-04 10:53:44 -0400

    created top-level authentication handler

--- main.py
+++ main.py
@@ -36,6 +36,22 @@ jinja_loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '
 jinja_env = jinja2.Environment(autoescape=True,
                                loader = jinja_loader)
 
+class AuthHandler(webapp2.RequestHandler):
+  def get(self, *args):
+    self.uid = auth_util.auth_into_site(self)
+    if self.uid:
+      self.aget(*args)
+    else:
+      self.redirect('/')
+  
+  def post(self, *args):
+    self.uid = auth_util.auth_into_site(self)
+    if self.uid:
+      self.apost(*args)
+    else:
+      self.redirect('/')
+    
+
 class MainHandler(webapp2.RequestHandler):
   def render(self):
     template = jinja_env.get_template('main.html')
@@ -48,6 +64,7 @@ class MainHandler(webapp2.RequestHandler):
       self.render()
 
 # deal with trailing /s
+
 class TrailingHandler(webapp2.RequestHandler):
   def get(self, path):
     self.redirect(path)
@@ -95,7 +112,6 @@ class LoginHandler(webapp2.RequestHandler):
           errors.append('Invalid Username/Password Combination')
 
       self.render(errors)
-
 
 class RegisterHandler(webapp2.RequestHandler):
   def render(self, errors = None):
@@ -152,7 +168,7 @@ class LogoutHandler(webapp2.RequestHandler):
     self.response.headers.add_header('Set-Cookie', 'auth=; Path=/')
     self.redirect('/')
 
-class PuzzlesHandler(webapp2.RequestHandler):
+class PuzzlesHandler(AuthHandler):
   def render(self, puzzles, completion):
     template = jinja_env.get_template('puzzles.html')
 
@@ -160,21 +176,17 @@ class PuzzlesHandler(webapp2.RequestHandler):
     self.response.out.write(template.render(puzzle_info = puzzle_info,
                                             logged_in = True))
 
-  def get(self):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      puzzles = puzzle_util.get_puzzles()
+  def aget(self):
+    puzzles = puzzle_util.get_puzzles()
 
-      puzzle_filter = lambda k: (user_util.user_can_view_puzzle(uid, k.key().id()) and not k.author == uid)
-      puzzles = filter(puzzle_filter, puzzles)
+    puzzle_filter = lambda k: (user_util.user_can_view_puzzle(self.uid, k.key().id()) and not k.author == self.uid)
+    puzzles = filter(puzzle_filter, puzzles)
 
-      completion = [puzzle_util.has_user_solved(uid, p.key().id()) for p in puzzles]
+    completion = [puzzle_util.has_user_solved(uid, p.key().id()) for p in puzzles]
 
-      self.render(puzzles, completion)
-    else:
-      self.redirect('/')
+    self.render(puzzles, completion)
 
-class OwnPuzzlesHandler(webapp2.RequestHandler):
+class OwnPuzzlesHandler(AuthHandler):
   def render(self, puzzles, completion):
     template = jinja_env.get_template('ownpuzzles.html')
 
@@ -182,21 +194,17 @@ class OwnPuzzlesHandler(webapp2.RequestHandler):
     self.response.out.write(template.render(puzzle_info = puzzle_info,
                                             logged_in = True))
 
-  def get(self):
-    uid = auth_util.auth_into_site(self)
-    
-    if uid:
-      puzzles = puzzle_util.get_puzzles()
+  def aget(self):  
+    puzzles = puzzle_util.get_puzzles()
 
-      puzzle_filter = lambda k: (k.author == uid)
-      puzzles = filter(puzzle_filter, puzzles)
+    puzzle_filter = lambda k: (k.author == self.uid)
+    puzzles = filter(puzzle_filter, puzzles)
 
-      completion = [puzzle_util.has_user_solved(uid, p.key().id()) for p in puzzles]
-      self.render(puzzles, completion)
-    else:
-      self.redirect('/')
+    completion = [puzzle_util.has_user_solved(self.uid, p.key().id()) for p in puzzles]
+    self.render(puzzles, completion)
 
-class PuzzleHandler(webapp2.RequestHandler):
+
+class PuzzleHandler(AuthHandler):
   def render(self, user, puzzle, up_info):
     template = jinja_env.get_template('puzzle.html')
     self.response.out.write(template.render(user = user, 
@@ -204,31 +212,26 @@ class PuzzleHandler(webapp2.RequestHandler):
                                             up_info = up_info,
                                             logged_in = True))
 
-  def get(self, short_code):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      puzzle = puzzle_util.get_puzzle_by_code(short_code)
-      pid = puzzle.key().id()
-      user = User.get_by_id(uid)
+  def aget(self, short_code):
+    puzzle = puzzle_util.get_puzzle_by_code(short_code)
+    pid = puzzle.key().id()
+    user = User.get_by_id(self.uid)
 
-      up_info = puzzle_util.get_upinfo(uid, pid)
+    up_info = puzzle_util.get_upinfo(self.uid, pid)
 
-      self.render(user, puzzle, up_info)
-    else:
-      self.redirect('/')
-
-class PuzzleSubmitAnswerHandler(webapp2.RequestHandler):
+    self.render(user, puzzle, up_info)
+      
+class PuzzleSubmitAnswerHandler(AuthHandler):
   # handler for submitted answers to puzzles
-  def get(self, short_code):
+  def aget(self, short_code):
     self.redirect('/puzzles/' + short_code)
 
-  def post(self, short_code):
+  def apost(self, short_code):
     answer = self.request.get('answer')
-    uid = auth_util.auth_into_site(self)
     puzzle = puzzle_util.get_puzzle_by_code(short_code)
     pid = puzzle.key().id()
 
-    up_info = puzzle_util.get_upinfo(uid, pid)
+    up_info = puzzle_util.get_upinfo(self.uid, pid)
     up_info.tries += 1
     
     if puzzle.answer == answer:
@@ -238,73 +241,67 @@ class PuzzleSubmitAnswerHandler(webapp2.RequestHandler):
 
     self.redirect('/puzzles/' + short_code) #temporary!
 
-class PuzzleSubmitPageHandler(webapp2.RequestHandler):
+class PuzzleSubmitPageHandler(AuthHandler):
   # handler for puzzle submission page
   def render(self, hunts):
     template = jinja_env.get_template('puzzle_submit.html')
     self.response.out.write(template.render(logged_in = 'True', 
 					     hunts = hunts))
 
-  def get(self):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      hunts = hunt_util.get_hunts_by_user(uid)
-      self.render(hunts)
+  def aget(self):
+    hunts = hunt_util.get_hunts_by_user(self.uid)
+    self.render(hunts)
 
-class PuzzleSubmitHandler(webapp2.RequestHandler):
-  def get(self):
+class PuzzleSubmitHandler(AuthHandler):
+  def aget(self):
     self.redirect('/puzzle_submit')
 
-  def post(self):
-    uid = auth_util.auth_into_site(self)
+  def apost(self):
     create_success = False
 
-    if uid:
-      title = self.request.get('title')
-      scode = self.request.get('scode')
-      answer = self.request.get('answer')
-      hunt = int(self.request.get('hunt'))
+    
+    title = self.request.get('title')
+    scode = self.request.get('scode')
+    answer = self.request.get('answer')
+    hunt = int(self.request.get('hunt'))
 
-      if title != '' and answer != '' and scode != '':
-        if hunt_util.is_user_author(uid, hunt) and not puzzle_util.code_used(scode):
-          puzzle = Puzzle(title = title,
-          short_code = scode,
-          answer = answer,
-          text = 'Insert puzzle text here',
-          author = uid,
-          hid = hunt,
-          approved = user_util.is_admin(uid))
+    if title != '' and answer != '' and scode != '':
+      if hunt_util.is_user_author(uid, hunt) and not puzzle_util.code_used(scode):
+	puzzle = Puzzle(title = title,
+	short_code = scode,
+	answer = answer,
+	text = 'Insert puzzle text here',
+	author = self.uid,
+	hid = hunt,
+	approved = user_util.is_admin(self.uid))
 
-          puzzle.put()
+	puzzle.put()
 
-          up_info = UserPuzzleInfo(uid = uid,
-                                   pid = puzzle.key().id(),
-                                   author = True,
-                                   solved = True)
-          up_info.put()
+	up_info = UserPuzzleInfo(uid = self.uid,
+				  pid = puzzle.key().id(),
+				  author = True,
+				  solved = True)
+	up_info.put()
 
-          create_success = True
+	create_success = True
           
-
-
     if create_success: 
       self.redirect('/puzzles/' + scode + '/edit')
     else:
       self.redirect('/puzzles')
  
-class PuzzleApproveHandler(webapp2.RequestHandler):
-  def get(self, short_code):
+class PuzzleApproveHandler(AuthHandler):
+  def aget(self, short_code):
     self.redirect('/puzzles/' + short_code)
 
-  def post(self, short_code):
-    uid = auth_util.auth_into_site(self)
+  def apost(self, short_code):
     pid = puzzle_util.get_puzzle_by_code(short_code).key().id()
 
-    puzzle_util.approve_puzzle(uid, pid)
+    puzzle_util.approve_puzzle(self.uid, pid)
 
     self.redirect('/puzzles/' + short_code)
 
-class PuzzleEditHandler(webapp2.RequestHandler):
+class PuzzleEditHandler(AuthHandler):
   def render(self, user, puzzle, puzzle_files, up_info):
     template = jinja_env.get_template('puzzle_edit.html')
     self.response.out.write(template.render(user = user, 
@@ -313,9 +310,7 @@ class PuzzleEditHandler(webapp2.RequestHandler):
                                             up_info = up_info,
                                             logged_in = True))
 
-  def get(self, short_code):
-    uid = auth_util.auth_into_site(self)
-    
+  def aget(self, short_code):
     puzzle = None
     while not puzzle:
       puzzle = puzzle_util.get_puzzle_by_code(short_code)
@@ -323,15 +318,15 @@ class PuzzleEditHandler(webapp2.RequestHandler):
     pid = puzzle.key().id()
     user = User.get_by_id(uid)
 
-    if user_util.user_can_edit_puzzle(uid, pid):
-      up_info = puzzle_util.get_upinfo(uid, pid)
+    if user_util.user_can_edit_puzzle(self.uid, pid):
+      up_info = puzzle_util.get_upinfo(self.uid, pid)
       puzzle_files = puzzle_util.get_puzzle_files(pid)
       self.render(user, puzzle, puzzle_files, up_info)
     else:
       self.redirect('/ownpuzzles')
 
-class PuzzleFileHandler(webapp2.RequestHandler):
-  def get(self, short_code, fname):
+class PuzzleFileHandler(AuthHandler):
+  def aget(self, short_code, fname):
     pfile = puzzle_util.get_puzzle_file_by_code(short_code, fname)
     if pfile:
       self.response.headers['Content-Type'] = pfile.mime_type.encode('ascii', 'ignore')
@@ -339,83 +334,70 @@ class PuzzleFileHandler(webapp2.RequestHandler):
     else:
       self.response.out.write('error: could not load file')
 
-class PuzzleEditSubmitHandler(webapp2.RequestHandler):
-  def get(self, short_code):
+class PuzzleEditSubmitHandler(AuthHandler):
+  def aget(self, short_code):
     self.redirect('/ownpuzzles')
 
-  def post(self, short_code):
-    uid = auth_util.auth_into_site(self)
+  def apost(self, short_code):
+    puzzle = puzzle_util.get_puzzle_by_code(short_code)
 
-    if uid:
-      puzzle = puzzle_util.get_puzzle_by_code(short_code)
+    if user_util.user_can_edit_puzzle(self.uid, puzzle.key().id()):
+      puzzle.title = self.request.get('title')
+      if self.request.get('uploadhtml'):
+	puzzle.text = self.request.get('uploadhtml')
+      else:
+	puzzle.text = self.request.get('input')
+      puzzle.answer = self.request.get('answer')
 
-      if user_util.user_can_edit_puzzle(uid, puzzle.key().id()):
-        puzzle.title = self.request.get('title')
-        if self.request.get('uploadhtml'):
-	  puzzle.text = self.request.get('uploadhtml')
-        else:
-	  puzzle.text = self.request.get('input')
-        puzzle.answer = self.request.get('answer')
-
-        puzzle.put()
+      puzzle.put()
 
 
     self.redirect('/ownpuzzles')
 
-class PuzzleEditUploadHandler(webapp2.RequestHandler):
+class PuzzleEditUploadHandler(AuthHandler):
   # handler for file submission
-  def get(self, short_code):
+  def aget(self, short_code):
     self.redirect('/ownpuzzles')
 
-  def post(self, short_code):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      puzzle = puzzle_util.get_puzzle_by_code(short_code)
-      if user_util.user_can_edit_puzzle(uid, puzzle.key().id()):
-	
-	pfile = self.request.get('uploadfile')
-	fname = self.request.POST['uploadfile'].filename
-	mime_type = puzzle_util.get_mime_type(fname.split('.')[-1])
+  def apost(self, short_code):
+    puzzle = puzzle_util.get_puzzle_by_code(short_code)
+    if user_util.user_can_edit_puzzle(self.uid, puzzle.key().id()):
+      
+      pfile = self.request.get('uploadfile')
+      fname = self.request.POST['uploadfile'].filename
+      mime_type = puzzle_util.get_mime_type(fname.split('.')[-1])
 
-	db_file = PuzzleFile(pid = puzzle.key().id(),
-			    fname = fname,
-			    mime_type = mime_type, 
-			    pfile = db.Blob(pfile))
-	db_file.put()
+      db_file = PuzzleFile(pid = puzzle.key().id(),
+			  fname = fname,
+			  mime_type = mime_type, 
+			  pfile = db.Blob(pfile))
+      db_file.put()
 
     self.redirect('/ownpuzzles')
 
-class HuntsHandler(webapp2.RequestHandler):
+class HuntsHandler(AuthHandler):
   def render(self, hunts):
     template = jinja_env.get_template('hunts.html')
 
     self.response.out.write(template.render(hunts = hunts, logged_in = True))
 
-  def get(self):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      hunts = [hunt for hunt in hunt_util.get_hunts() if hunt.author != uid]
-      self.render(hunts)
-    else:
-      self.redirect('/')
+  def aget(self):
+    hunts = [hunt for hunt in hunt_util.get_hunts() if hunt.author != self.uid]
+    self.render(hunts)
 
-class OwnHuntsHandler(webapp2.RequestHandler):
+class OwnHuntsHandler(AuthHandler):
   def render(self, hunts):
     template = jinja_env.get_template('ownhunts.html')
 
     self.response.out.write(template.render(hunts = hunts, logged_in = True))
 
-  def get(self):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      hunts = hunt_util.get_hunts_by_user(uid)
+  def aget(self):
+    hunts = hunt_util.get_hunts_by_user(self.uid)
 
-      self.render(hunts)
-    else:
-      self.redirect('/')
+    self.render(hunts)
 
 
-class HuntHandler(webapp2.RequestHandler):
+class HuntHandler(AuthHandler):
   def render(self, hunt, puzzles, completion):
     template = jinja_env.get_template('hunt.html')
 
@@ -424,54 +406,44 @@ class HuntHandler(webapp2.RequestHandler):
 					     puzzle_info = puzzle_info,
 					     logged_in = True))
 
-  def get(self, short_code):
-    uid = auth_util.auth_into_site(self)
-    
-    if uid:
-      hunt = hunt_util.get_hunt_by_code(short_code)
-      puzzles = hunt_util.get_puzzles_of_hunt(hunt.key().id())
+  def aget(self, short_code):
+    hunt = hunt_util.get_hunt_by_code(short_code)
+    puzzles = hunt_util.get_puzzles_of_hunt(hunt.key().id())
 
-      completion = [puzzle_util.has_user_solved(uid, p.key().id()) for p in puzzles]
+    completion = [puzzle_util.has_user_solved(self.uid, p.key().id()) for p in puzzles]
 
-      self.render(hunt, puzzles, completion)
+    self.render(hunt, puzzles, completion)
 
-class HuntCreateHandler(webapp2.RequestHandler):
+class HuntCreateHandler(AuthHandler):
   def render(self):
     template = jinja_env.get_template('create_hunt.html')
 
     self.response.out.write(template.render(logged_in = True))
 
-  def get(self):
-    uid = auth_util.auth_into_site(self)
+  def aget(self):
+    self.render()
     
-    if uid:
-      self.render()
-    else:
-      self.redirect('/')
 
-class HuntCreateSubmitHandler(webapp2.RequestHandler):
-  # we should really refactor this stuff soon
-  def get(self):
+class HuntCreateSubmitHandler(AuthHandler):
+  def aget(self):
     self.redirect('/create_hunt')
 
-  def post(self):
-    uid = auth_util.auth_into_site(self)
+  def apost(self):
     title = self.request.get('title')
     short_code = self.request.get('short_code')
 
-    if uid and title != '' and short_code != '' and not puzzle_util.code_used(scode):
+    if self.uid and title != '' and short_code != '' and not puzzle_util.code_used(scode):
       hunt = PuzzleHunt(title = title,
                         short_code = short_code,
-                        author = uid)
+                        author = self.uid)
 
       hunt.put()
 
       self.redirect('/hunts/%s/edit' % short_code)
-
     else:
       self.redirect('/create_hunt')
 
-class HuntEditHandler(webapp2.RequestHandler):
+class HuntEditHandler(AuthHandler):
   def render(self, user, hunt, puzzles):
     template = jinja_env.get_template('edit_hunt.html')
 
@@ -480,38 +452,31 @@ class HuntEditHandler(webapp2.RequestHandler):
 					     puzzles = puzzles,
                                             logged_in = True))
 
-  def get(self, short_code):
-    uid = auth_util.auth_into_site(self)
-    if uid:
-      hunt = None
-      while not hunt:
-	hunt = hunt_util.get_hunt_by_code(short_code)
-	
-      hid = hunt.key().id()
-      user = User.get_by_id(uid)
+  def aget(self, short_code):
+    hunt = None
+    while not hunt:
+      hunt = hunt_util.get_hunt_by_code(short_code)
+      
+    hid = hunt.key().id()
+    user = User.get_by_id(self.uid)
 
-      if user_util.user_can_edit_hunt(uid, hid): 
-	puzzles = hunt_util.get_puzzles_of_hunt(hid)
-	self.render(user, hunt, puzzles)
-      else:
-	self.redirect('/ownhunts')
+    if user_util.user_can_edit_hunt(self.uid, hid): 
+      puzzles = hunt_util.get_puzzles_of_hunt(hid)
+      self.render(user, hunt, puzzles)
     else:
-      self.redirect('/')
+      self.redirect('/ownhunts')
 
-class HuntEditSubmitHandler(webapp2.RequestHandler):
-  def get(self, short_code):
+class HuntEditSubmitHandler(AuthHandler):
+  def aget(self, short_code):
     self.redirect('/hunts')
 
-  def post(self, short_code):
-    uid = auth_util.auth_into_site(self)
+  def apost(self, short_code):
+    hunt = hunt_util.get_hunt_by_code(short_code)
 
-    if uid:
-      hunt = hunt_util.get_hunt_by_code(short_code)
+    if user_util.user_can_edit_hunt(self.uid, hunt.key().id()):
+      hunt.title = self.request.get('title')
 
-      if user_util.user_can_edit_hunt(uid, hunt.key().id()):
-        hunt.title = self.request.get('title')
-
-        hunt.put()
+      hunt.put()
 
     self.redirect('/ownhunts')
 
