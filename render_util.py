646d2d5ec9a0a716a1b28124022f0b51d3632b80
Jon Schneider <jon@jon-Alienware-X51.(none)>
2013-04-10 02:49:51 -0400

    wip: added render_util

--- render_util.py
+++ render_util.py
@@ -7,3 +7,19 @@ import re
 import puzzle_util
 import hunt_util
 
+def render_page(user, page):
+  tokens = split_page(page)
+  codes = [parse_tag(tag)[1] for tag in tokens if is_tag(tag)]
+  
+
+def split_page(page):
+  exp = '(\[\[\[[SE]:\w+\]\]\])'
+  return re.split(exp, page)
+
+def is_tag(token):
+  return token[:3] == '[[[' and token[-3:] = ']]]'
+
+def parse_tag(tag):
+  return token[3:-3].split(':')
+
+
